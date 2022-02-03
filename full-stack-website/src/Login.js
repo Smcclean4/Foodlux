8f6ea3b40d2449c25ff93e6bdd35f07f7e311f65
Sidney McClean <sidney.mcclean@outlook.com>
2022-02-02 23:17:27 -0800

    material ui
    installed material ui to style log in page

--- full-stack-website/src/Login.js
+++ full-stack-website/src/Login.js
@@ -1,11 +1,41 @@
-import React from 'react'
-import './Login.css'
+import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './Login.css';
 
 const Login = () => {
 
     return (
         <div className="login-background">
-            <h1>FOOD DELIVERY SERVICE! ;)</h1>
+            <p className="login-logo">Foodlux</p>
+            <Box
+                component="form"
+                sx={{
+                    border: "1px solid black",
+                    borderRadius: "25px 25px",
+                    backgroundColor: "white",
+                    boxShadow: "black 4px 2px 10px",
+                    width: "25%",
+                    margin: "0 auto",
+                    height: "50%",
+                }}
+                action="/"
+                method="post"
+                target="_blank"
+                autocomplete="on"
+            >
+            <TextField className="login-fields" label="Username" margin="dense" variant="outlined" />
+                <br></br>
+                <br></br>
+            <TextField className="login-fields" label="Email" type="email" margin="dense" variant="outlined" />
+                <br></br>
+                <br></br>
+            <TextField required className="login-fields" label="Password" type="password" margin="dense" variant="filled" />
+            <br></br>
+                <br></br>
+            <Button variant="contained">Log In</Button>
+            </Box>
         </div>
     )
 }
