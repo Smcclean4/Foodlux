7f98a0625fb792a3388da3feba8f272ec6da20e1
Sidney McClean <sidney.mcclean@outlook.com>
2022-02-22 19:13:41 -0800

    update

--- full-stack-website/src/routes/Home.js
+++ full-stack-website/src/routes/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom';
 import '../Home.css'
 
 const Home = () => {
-
+    // get businesses from database and set them into their section
+    const [company, setCompany] = useState([])
+    // get menu items from database and set items into items section
+    const [items, setItems] = useState([])
 
     return (
         <div className="home-background">
