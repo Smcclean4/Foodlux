f32d727dfcb6c25f2b00d2a59c5aaa8ca68c4bc3
Sidney McClean <sidney.mcclean@outlook.com>
2022-02-25 12:25:10 -0800

    update
    adding items to different sections.

--- full-stack-website/src/routes/Home.js
+++ full-stack-website/src/routes/Home.js
@@ -4,9 +4,9 @@ import '../Home.css'
 
 const Home = () => {
     // get businesses from database and set them into their section
-    const [fastfood, setFastfood] = useState([])
-    const [snacks, setSnacks] = useState([])
-    const [finedine, setFinedine] = useState([])
+    const [fastfood, setFastfood] = useState(["Bobs Burgers", "Koala Express", "Shauns Tacos", "Firecracker Fast Sushi"])
+    const [snacks, setSnacks] = useState(["8-11", "PVS", "Cents Tree", "Baldi"])
+    const [finedine, setFinedine] = useState(["Renihana", "Puths Chriss", "GLE"])
     // get menu items from database and set items into items section
     const [ffitems, setFfitems] = useState([],[])
     const [snitems, setSnitems] = useState([],[])
