c4fb628a04f9b1acca8e932310159013718d99a4
Smcclean4 <lsaudim5@outlook.com>
2023-03-08 16:06:47 -0800

    update

--- full-stack-website/src/containers/Home/Home.test.ts
+++ full-stack-website/src/containers/Home/Home.test.ts
@@ -8,14 +8,17 @@ import UserData from "./Home";
 import setValue from "./Home";
 
 describe('confirming that functions and variables do what they are supposed to in Home', () => {
-  test('checking if itemsExists is truthy', () => {
-    expect(itemExists).toBeTruthy()
+  test('checks whether an item exists in the cart', () => {
+    expect(itemExists).toBeTruthy();
+    expect(itemExists).not.toBeUndefined();
   })
   test('checking if setRender is truthy', () => {
     expect(setRender).toBeTruthy()
+    expect(setRender).not.toBeUndefined();
   })
   test('checking if setLoading is truthy', () => {
     expect(setLoading).toBeTruthy()
+    expect(setLoading).not.toBeUndefined();
   })
   test('checking if Categories contains some sort of value', () => {
     expect(Categories).toBeDefined();
