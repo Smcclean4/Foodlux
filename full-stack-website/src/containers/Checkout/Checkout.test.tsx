a3888989512caaa8d02403c7620e498534d11a55
Smcclean4 <lsaudim5@outlook.com>
2023-03-30 21:48:25 -0700

    testing for foodlux
    finished testing all components and containers.

--- full-stack-website/src/containers/Checkout/Checkout.test.tsx
+++ full-stack-website/src/containers/Checkout/Checkout.test.tsx
@@ -1,19 +1,14 @@
-import axios from 'axios';
 import React from 'react';
 import Checkout from './Checkout';
 import {
-// fireEvent,
-render,
-// waitFor 
+	render
 } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { UserEmailInfoInterface } from './Checkout';
 import '@testing-library/jest-dom';
 
-// jest.mock('axios');
-
 describe('creating tests for checkout container', () => {
-	const exampleUserEmailInfo = {
+	const exampleUserEmailInfo: UserEmailInfoInterface = {
 		firstname: 'test',
 		country: 'USA',
 		city: 'Compton',
@@ -31,32 +26,6 @@ describe('creating tests for checkout container', () => {
 		email: expect.any(String)
 	}
 
-	// test('handles form submission', async () => {
-	// 	const exampleUserEmailInfo: UserEmailInfoInterface = {
-	// 		firstname: "John",
-	// 		country: "USA",
-	// 		city: "Los Angeles",
-	// 		state: "California",
-	// 		zip: "90210",
-	// 		email: "john@example.com",
-	// 	};
-
-	// 	const { getByTestId } = render(<Checkout />, { wrapper: MemoryRouter });
-	// 	fireEvent.change(getByTestId('firstname-input'), { target: { value: exampleUserEmailInfo.firstname } });
-	// 	fireEvent.change(getByTestId('country-input'), { target: { value: exampleUserEmailInfo.country } });
-	// 	fireEvent.change(getByTestId('city-input'), { target: { value: exampleUserEmailInfo.city } });
-	// 	fireEvent.change(getByTestId('state-input'), { target: { value: exampleUserEmailInfo.state } });
-	// 	fireEvent.change(getByTestId('zip-input'), { target: { value: exampleUserEmailInfo.zip } });
-	// 	fireEvent.change(getByTestId('email-input'), { target: { value: exampleUserEmailInfo.email } });
-	// 	fireEvent.submit(getByTestId('Email'));
-
-	// 	await waitFor(() => expect(axios.post).toHaveBeenCalledTimes(1))
-	// 	expect(axios.post).toHaveBeenCalledWith(
-	// 		`http://localhost:${process.env.REACT_APP_PORT}/sendEmail`,
-	// 		exampleUserEmailInfo
-	// 	);
-	// });
-
 	test('making sure that checkout renders', () => {
 		const { getByTestId } = render(<Checkout />, { wrapper: MemoryRouter })
 		let checkoutTest = getByTestId('Checkout')
@@ -69,7 +38,6 @@ describe('creating tests for checkout container', () => {
 		expect(cartButton).toBeInTheDocument();
 		expect(cartButton).toBeDefined();
 	})
-
 
 	test('testing to see if info that is being submitted is adhering to user email info interface', () => {
 		expect(exampleUserEmailInfo).toMatchObject<UserEmailInfoInterface>(exampleUserEmailInfoInterface)
